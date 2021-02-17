f232f0c59964085b706c86b4904d711664c819b9
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-02-17 12:33:32 -0800

    added code to task 3, did not work lol

--- index.js
+++ index.js
@@ -65,9 +65,11 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
-}
+function is31Flavors(arr){
+   console.log(originalFlavors.length)
+   if (originalFlavors.length > 31);
+   return false;
+   }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
@@ -82,9 +84,10 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-   /*your code here*/
+function addFlavor(arr){
+   arr.unshift('Rainbow Sherbert');
 }
+console.log(originalFlavors);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
